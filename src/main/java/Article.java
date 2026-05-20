5eec383beff0b73787949d7bf330a7c52b314d7c
leona <rax3zgd@gmail.com>
2026-05-20 19:13:33 +0200

    codi original

---
+++ src/main/java/Article.java
@@ -0,0 +1,12 @@
+class Article {
+    public String nom;
+    public int diesPerVendre;
+    public int qualitat;
+
+    public Article(String nom, int diesPerVendre, int qualitat) {
+        this.nom = nom;
+        this.diesPerVendre = diesPerVendre;
+        this.qualitat = qualitat;
+    }
+}
+
